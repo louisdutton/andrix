b8e965c5918e9de3af905de5ae2c4c82679ce51d
Louis Dutton <louis.dutton@travelchapter.com>
2024-08-04 11:51:43 +0000

    move to kotlin-test for unit tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
   implementation("androidx.compose.ui:ui-graphics")
   implementation("androidx.compose.ui:ui-tooling-preview")
   implementation("androidx.compose.material3:material3")
-  testImplementation("junit:junit:4.13.2")
+  testImplementation(kotlin("test"))
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
   androidTestImplementation(platform("androidx.compose:compose-bom:2024.06.00"))
