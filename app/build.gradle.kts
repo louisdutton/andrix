a03e8cf240b783d096511bd335444384db2214cb
Louis Dutton <louis.dutton@travelchapter.com>
2024-08-04 08:26:55 +0000

    init

---
+++ app/build.gradle.kts
@@ -0,0 +1,76 @@
+plugins {
+  id("com.android.application")
+  id("org.jetbrains.kotlin.android")
+}
+
+val versionMajor = 1
+val versionMinor = 0
+val versionPatch = 0
+val versionBuild = 0 // Bump for dogfood builds, public betas, etc.
+
+val bundleId = "com.delasign.samplestarterproject"
+
+android {
+  namespace = bundleId
+  compileSdk = 34
+
+  defaultConfig {
+    applicationId = bundleId
+    minSdk = 31
+    targetSdk = 34
+    versionCode = versionMajor * 10000 + versionMinor * 1000 + versionPatch * 100 + versionBuild
+    versionName = "${versionMajor}.${versionMinor}.${versionPatch}"
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    vectorDrawables { useSupportLibrary = true }
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = true
+      isShrinkResources = true
+
+      // Includes the default ProGuard rules files that are packaged with
+      // the Android Gradle plugin. To learn more, go to the section about
+      // R8 configuration files.
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+  }
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+  }
+  kotlinOptions { jvmTarget = "21" }
+  buildFeatures { compose = true }
+  composeOptions { kotlinCompilerExtensionVersion = "1.4.5" }
+  packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
+}
+
+dependencies {
+  implementation("androidx.core:core-ktx:1.10.1")
+  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+  implementation("androidx.activity:activity-compose:1.7.2")
+  implementation(platform("androidx.compose:compose-bom:2022.10.00"))
+  implementation("androidx.compose.ui:ui")
+  implementation("androidx.compose.ui:ui-graphics")
+  implementation("androidx.compose.ui:ui-tooling-preview")
+  implementation("androidx.compose.material3:material3")
+  testImplementation("junit:junit:4.13.2")
+  androidTestImplementation("androidx.test.ext:junit:1.1.5")
+  androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+  androidTestImplementation(platform("androidx.compose:compose-bom:2022.10.00"))
+  androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+  debugImplementation("androidx.compose.ui:ui-tooling")
+  debugImplementation("androidx.compose.ui:ui-test-manifest")
+  // GSON
+  implementation("com.google.code.gson:gson:2.8.9")
+  // Constraint Layout
+  implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
+  // Material Symbols
+  implementation("androidx.compose.material:material-icons-extended:1.4.3")
+  // Datastore
+  implementation("androidx.datastore:datastore-preferences:1.0.0")
+  implementation("androidx.datastore:datastore-preferences-rxjava2:1.0.0")
+  implementation("androidx.datastore:datastore-preferences-rxjava3:1.0.0")
+}
---
+++ app/src/main/java/com/delasign/samplestarterproject/ui/styleguide/theme/Type.kt
@@ -0,0 +1,43 @@
+package com.delasign.samplestarterproject.ui.styleguide.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.delasign.samplestarterproject.R
+
+// CREATE THE FONT FAMILY
+val Helvetica = FontFamily(
+    Font(R.font.helvetica_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.helvetica, FontWeight.Normal, FontStyle.Normal),
+)
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp,
+    ),
+    /* Other default text styles to override
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+     */
+)
