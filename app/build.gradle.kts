704d1b69fed55bcb12f0f58cf0aaa90d39c042c7
Louis Dutton <louis.dutton@travelchapter.com>
2024-08-04 10:04:33 +0000

    bump deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,15 +43,15 @@ android {
   }
   kotlinOptions { jvmTarget = "21" }
   buildFeatures { compose = true }
-  composeOptions { kotlinCompilerExtensionVersion = "1.4.5" }
+  composeOptions { kotlinCompilerExtensionVersion = "1.5.14" }
   packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
 }
 
 dependencies {
-  implementation("androidx.core:core-ktx:1.10.1")
-  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-  implementation("androidx.activity:activity-compose:1.7.2")
-  implementation(platform("androidx.compose:compose-bom:2022.10.00"))
+  implementation("androidx.core:core-ktx:1.13.1")
+  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+  implementation("androidx.activity:activity-compose:1.9.1")
+  implementation(platform("androidx.compose:compose-bom:2024.06.00"))
   implementation("androidx.compose.ui:ui")
   implementation("androidx.compose.ui:ui-graphics")
   implementation("androidx.compose.ui:ui-tooling-preview")
@@ -59,7 +59,7 @@ dependencies {
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-  androidTestImplementation(platform("androidx.compose:compose-bom:2022.10.00"))
+  androidTestImplementation(platform("androidx.compose:compose-bom:2024.06.00"))
   androidTestImplementation("androidx.compose.ui:ui-test-junit4")
   debugImplementation("androidx.compose.ui:ui-tooling")
   debugImplementation("androidx.compose.ui:ui-test-manifest")
@@ -70,7 +70,7 @@ dependencies {
   // Material Symbols
   implementation("androidx.compose.material:material-icons-extended:1.4.3")
   // Datastore
-  implementation("androidx.datastore:datastore-preferences:1.0.0")
-  implementation("androidx.datastore:datastore-preferences-rxjava2:1.0.0")
-  implementation("androidx.datastore:datastore-preferences-rxjava3:1.0.0")
+  implementation("androidx.datastore:datastore-preferences:1.1.1")
+  implementation("androidx.datastore:datastore-preferences-rxjava2:1.1.1")
+  implementation("androidx.datastore:datastore-preferences-rxjava3:1.1.1")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,6 @@ buildscript { repositories { google() } }
 plugins {
   id("com.android.application") version "8.5.0" apply false
   id("com.android.library") version "8.5.0" apply false
-  id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+  id("org.jetbrains.kotlin.android") version "1.9.24" apply false
   id("com.github.ben-manes.versions") version "0.51.0"
 }
