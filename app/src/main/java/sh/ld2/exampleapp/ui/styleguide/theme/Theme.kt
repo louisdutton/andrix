a9ee9af8cc58e59ad91f04df5005163edf59b7cc
Louis Dutton <louis.dutton@travelchapter.com>
2024-08-04 12:42:21 +0000

    move to dynamic colors with jetpack compose

--- app/src/main/java/sh/ld2/exampleapp/ui/styleguide/theme/Theme.kt
+++ app/src/main/java/sh/ld2/exampleapp/ui/styleguide/theme/Theme.kt
@@ -1,61 +1,39 @@
 package sh.ld2.example.ui.styleguide.theme
 
-import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
+import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    background = Black,
-    primary = White,
-)
+private val DarkColorScheme =
+    darkColorScheme(
+        background = Black,
+        primary = White,
+    )
 
-private val LightColorScheme = lightColorScheme(
-    background = White,
-    primary = Black,
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    secondary = Color(0xFFFFFBFE),
-    tertiary = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-     */
-)
+private val LightColorScheme =
+    lightColorScheme(
+        background = White,
+        primary = Black,
+    )
 
 @Composable
 fun AppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    isDarkTheme: Boolean = isSystemInDarkTheme(),
+    isDynamicColor: Boolean = true,
     content: @Composable () -> Unit,
 ) {
-    val colorScheme = when {
-        darkTheme -> DarkColorScheme
+  val colorScheme =
+      when {
+        isDynamicColor && isDarkTheme -> dynamicDarkColorScheme(LocalContext.current)
+        isDynamicColor && !isDarkTheme -> dynamicLightColorScheme(LocalContext.current)
+        isDarkTheme -> DarkColorScheme
         else -> LightColorScheme
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
-    }
+      }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content,
-    )
+  MaterialTheme(
+      colorScheme = colorScheme,
+      typography = Typography,
+      content = content,
+  )
 }
